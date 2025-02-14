db8c027dfc57cb5d99fba0bc2ec005ad7134ee57
Atra <atraa0621@gmail.com>
2025-02-14 13:35:48 +0800

    来单提醒业务代码开发

--- Sky/sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
+++ Sky/sky-take-out/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
 @EnableCaching //开启注解方式的缓存管理
+@EnableScheduling  // 添加此注解来启用定时任务
 public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);

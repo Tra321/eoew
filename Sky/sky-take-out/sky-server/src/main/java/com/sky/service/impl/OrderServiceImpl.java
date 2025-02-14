eaf85becb8e677a4b967041bd44c461dc8a2e251
Atra <atraa0621@gmail.com>
2025-02-14 19:54:11 +0800

    客户催单业务代码开发

--- Sky/sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ Sky/sky-take-out/sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -593,4 +593,26 @@ public class OrderServiceImpl implements OrderService {
 
         orderMapper.update(orders);
     }
+
+    /**
+     * 客户催单
+     * @param id
+     */
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        Map map = new HashMap();
+        map.put("type",2); // 1 代表来单提醒 2 表示客户催单
+        map.put("orderId",id);
+        map.put("content","订单号："+ ordersDB.getNumber());
+
+        // 通过websocket向客户端浏览器推送消息 type orderId content
+        webSocketServer.sendToAllClient(JSON.toJSONString(map));
+    }
 }
